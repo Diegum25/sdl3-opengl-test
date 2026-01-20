97bc2a14473b7b8993794bad64dead7dc50e92c6
diego <diegoschacht25@gmail.com>
2026-01-19 19:55:56 -0600

    implemented nicely

--- hello.c
+++ hello.c
@@ -9,25 +9,34 @@
   including commercial applications, and to alter it and redistribute it
   freely.
 */
+
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <glad/glad.h>
 
-int main(int argc, char* argv[]){
+/*
+ * This example code $WHAT_IT_DOES.
+ *
+ * This code is public domain. Feel free to use it for any purpose!
+ */
 
-    // Create a window data type
-    // This pointer will point to the 
-    // window that is allocated from SDL_CreateWindow
-    SDL_Window* window=NULL;
 
-    // Initialize the video subsystem.
-    // If it returns less than 1, then an
-    // error code will be received.
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        SDL_Log("SDL could not be initialized: ");
-    }else{
-        SDL_Log("SDL video system is ready to go\n");
+
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    SDL_SetAppMetadata("Example HUMAN READABLE NAME", "1.0", "com.example.CATEGORY-NAME");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
+
     // Before we create our window, specify OpenGL version
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
@@ -36,13 +45,12 @@ int main(int argc, char* argv[]){
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,24);
 
-    // Request a window to be created for our platform
-    // The parameters are for the title, x and y position,
-    // and the width and height of the window.
-    window = SDL_CreateWindow("C SDL3 Window",
-            640,
-            480,
-            SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow("SDL3-OPENGL-TEST", 640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+
+    if (!window) {
+        SDL_Log("Couldn't create window: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
 
     // OpenGL setup the graphics context
     SDL_GLContext context;
@@ -51,51 +59,31 @@ int main(int argc, char* argv[]){
     // Setup our function pointers
     gladLoadGLLoader(SDL_GL_GetProcAddress);
 
-    // Infinite loop for our application
-    bool gameIsRunning = true;
-    while(gameIsRunning){
-        glViewport(0,0,640,480);
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
 
-        SDL_Event event;
-        // Start our event loop
-        while(SDL_PollEvent(&event)){
-            // Handle each specific event
-            if(event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED){
-                SDL_Log("Close window event\n");
-                gameIsRunning= false;
-            }
-            if(event.type == SDL_EVENT_MOUSE_MOTION){
-                SDL_Log("Mpuse has been moved\n");
-            }
-            if(event.type == SDL_EVENT_KEY_DOWN){
-                SDL_Log("a key has been pressed\n");
-                if(event.key.key == SDLK_0){
-                    SDL_Log("0 was pressed\n");
-                }else{
-                    SDL_Log("0 was not pressed\n");
-                }
-            }
-            // Retrieve the state of all of the keys
-            // Then we can query the scan code of one or more
-            // keys at a time
-            const Uint8* state = SDL_GetKeyboardState(NULL);
-            if(state[SDL_SCANCODE_RIGHT]){
-                SDL_Log("right arrow key is pressed\n");
-            }
-        }
-
-        glClearColor(1.0f,0.0f,0.0f,1.0f);
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-        SDL_GL_SwapWindow(window);
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
 
-    // We destroy our window. We are passing in the pointer
-    // that points to the memory allocated by the 
-    // 'SDL_CreateWindow' function. Remember, this is
-    // a 'C-style' API, we don't have destructors.
-    SDL_DestroyWindow(window);
-    // our program.
-    SDL_Quit();
-    return 0;
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    glViewport(0,0,640,480);
+    glClearColor(1.0f,0.0f,0.0f,1.0f);
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
