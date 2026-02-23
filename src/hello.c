38f21ea2af90aa16116ffd8bd627fa92c8c1ab60
diego <diegoschacht25@gmail.com>
2026-02-23 15:04:46 -0600

    fixed shader

--- src/hello.c
+++ src/hello.c
@@ -217,15 +217,14 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     glBindBuffer(GL_ARRAY_BUFFER,VBO3);
     glBufferData(GL_ARRAY_BUFFER,sizeof(float) * objCube.vertsAmnt, objCube.verts,GL_STATIC_DRAW);
 
-    for (int i = 0 ; i < objCube.vertsAmnt; i++){
-        printf("v: %f\n",objCube.verts[i]);
-    }
-
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO3);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * objCube.indexAmnt, objCube.indexes,GL_STATIC_DRAW);
 
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE, 3 * sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
+
+    free(objCube.verts);
+    free(objCube.indexes);
 
     glBindVertexArray(0);
 
--- testing/simpleFShader.glsl
+++ testing/simpleFShader.glsl
@@ -2,17 +2,8 @@
 out vec4 FragColor;
 
 uniform vec3 lightColor;
-in vec3 FragPos;
 
 void main()
 {
-    // Compute normal from triangle surface
-    vec3 dx = dFdx(FragPos);
-    vec3 dy = dFdy(FragPos);
-    vec3 normal = normalize(cross(dx, dy));
-
-    // Convert from [-1,1] to [0,1] for color
-    vec3 color = normal * 0.5 + 0.5;
-
-    FragColor = vec4(color, 1.0);
+   FragColor = vec4(lightColor, 1.0f);
 }
