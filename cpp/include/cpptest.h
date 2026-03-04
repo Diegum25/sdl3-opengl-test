22bed60c21713998a3ec9630acd70e0b7cf5ae5f
diego <diegoschacht25@gmail.com>
2026-03-04 11:10:50 -0600

    symlink test

--- cpp/include/cpptest.h
+++ cpp/include/cpptest.h
@@ -1,14 +1,1 @@
-#pragma once
-
-#ifdef __cplusplus
-extern "C"{
-#endif
-
-typedef struct TestClass TestClass; // class to struct?
-
-TestClass* TestClass_Create();
-int TestClass_GetCoolInt(TestClass*);
-
-#ifdef __cplusplus
-}
-#endif
+include/cpptest.h
