61122212a975084cd864ac1ed087ce999d3dfbdb
Diegum25 <diegoschacht25@gmail.com>
2026-04-11 22:07:28 -0600

    deleted shit

--- include/rmi_main.h
+++ include/rmi_main.h
@@ -1,4 +1,4 @@
-#ifdef RMI_MAIN // this makes clangd stop complaining about something
+#ifdef RMI_MAIN // this makes clangd stop complaining about some stuff
     #ifndef RMI_MAIN_INCLUDED
         #define RMI_MAIN_INCLUDED
 
@@ -10,18 +10,12 @@
         // SDL ALSO COMES WITH STB_IMAGE BUT IDK HOW TO USE IT
         #define STB_IMAGE_IMPLEMENTATION // DONT PUT THIS ANYWHERE ELSE
         #include "stb_image.h"
-
-        #include "sceneMatrix.h"
-        #include "shader.h"
-        #include "camera.h"
-        #include "scene.h"
+        
         #include "test.h"
 
         /* We will use this renderer to draw into this window every frame. */
         static SDL_Window *window = NULL;
         static void* RMIAppState = NULL; // sdl does this the same way: https://github.com/libsdl-org/SDL/blob/main/src/main/SDL_main_callbacks.c
-
-        RMI_Scene scene;
 
         SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         {
@@ -59,9 +53,6 @@
             // Setup our function pointers
             gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress); // weird thing <- ****** ai wrote this shit
 
-            // stuff
-            RMIInitScene(&scene,window);
-
             // uncomment this call to draw in wireframe polygons.
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
@@ -83,41 +74,16 @@
                 return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
             }
             if (event->type == SDL_EVENT_MOUSE_WHEEL){
-                /* SDL_Log("change X:%f\n",event->wheel.x);
-                SDL_Log("change Y:%f\n",event->wheel.y); */
-                scene.camera.fov += event->wheel.y;
+
             }
             if (event->type == SDL_EVENT_MOUSE_MOTION){
-                float changeX = event->motion.xrel * 0.15f;
-                float changeY = -event->motion.yrel * 0.15f;
-                scene.camera.yaw += changeX;
 
-                //SDL_Log("yaw: %f\n",scene.camera.yaw);
-
-                scene.camera.pitch = glm_clamp(scene.camera.pitch + changeY,-89.0f,89.0f);
-
-                scene.camera.front[0] = SDL_cos(glm_rad(scene.camera.yaw)) * SDL_cos(glm_rad(scene.camera.pitch));
-                scene.camera.front[1] = SDL_sin(glm_rad(scene.camera.pitch));
-                scene.camera.front[2] = SDL_sin(glm_rad(scene.camera.yaw)) * SDL_cos(glm_rad(scene.camera.pitch));
-                glm_normalize(scene.camera.front);
             }
             if (event->type == SDL_EVENT_KEY_DOWN){
                 switch (event->key.key)
                 {
                 case SDLK_ESCAPE:
                     return SDL_APP_SUCCESS;
-                    break;
-                case SDLK_B:
-                    SDL_SetWindowRelativeMouseMode(window,true);
-                    break;
-                case SDLK_V:
-                    SDL_SetWindowRelativeMouseMode(window,false);
-                    break;
-                case SDLK_N:
-                    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-                    break;
-                case SDLK_M:
-                    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);    
                     break;
                 default:
                     break;
@@ -129,37 +95,8 @@
         /* This function runs once per frame, and is the heart of the program. */
         SDL_AppResult SDL_AppIterate(void *appstate)
         {
-            //printf("hi the appstate points to %d\n",*(int*)appstate);
-            const bool* key_states = SDL_GetKeyboardState(NULL);
-            const float speed = -0.1f;
-            vec2 moveDir = {0.0f,0.0f};
-
-            if (key_states[SDL_SCANCODE_W]){
-                vec2 add = {0.0f,1.0f};
-                glm_vec2_add(moveDir,add,moveDir);
-            }
-            if (key_states[SDL_SCANCODE_S]){
-                vec2 add = {0.0f,-1.0f};
-                glm_vec2_add(moveDir,add,moveDir);
-            }
-            if (key_states[SDL_SCANCODE_A]){
-                vec2 add = {-1.0f,0.0f};
-                glm_vec2_add(moveDir,add,moveDir);
-            }
-            if (key_states[SDL_SCANCODE_D]){
-                vec2 add = {1.0f,0.0f};
-                glm_vec2_add(moveDir,add,moveDir);
-            }
-
-            glm_vec2_normalize(moveDir); // emulate circular analog stick
-
-            RMICameraFlight(&scene.camera,moveDir);
-
             int x,y;
             SDL_GetWindowSizeInPixels(window,&x,&y);
-
-            glm_perspective(glm_rad(scene.camera.fov),(float)x / (float)y, 0.1f,100.0f, scene.matrix.projection);
-            glm_lookat(scene.camera.position,scene.camera.view,scene.camera.up,scene.matrix.view);
 
             glViewport(0,0,x,y);
             glClearColor(0.1f,0.1f,0.1f,1.0f);
--- tests/main.c
+++ tests/main.c
@@ -2,15 +2,9 @@
 #include "rmi_main.h" // this also includes a bunch of slop :D
 #include "rmi.h"
 
-typedef struct{
-    obj_data data;
-} globalStupidStuff;
 
 void RMI_Init(void** appstate){
-    *appstate = SDL_malloc(sizeof(globalStupidStuff));
-    globalStupidStuff* helper = *(globalStupidStuff**)appstate;
-    helper->data = getObjData("const char *objPath");
-    SDL_Log("%s\n",helper->data.fileName);
+
 }
 
 void RMI_Update(void* appstate){
@@ -18,7 +12,5 @@ void RMI_Update(void* appstate){
 }
 
 void RMI_End(void *appstate){
-    globalStupidStuff* helper = (globalStupidStuff*)appstate;
-    SDL_Log("%s\n",helper->data.fileName);
-    SDL_free(helper->data.verts);
+
 }
